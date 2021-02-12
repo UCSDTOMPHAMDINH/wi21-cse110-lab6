25d491ada9c1b2a5129c9b3adee16c2e32ca4b02
UCSDTOMPHAMDINH <59636279+UCSDTOMPHAMDINH@users.noreply.github.com>
2021-02-11 22:04:01 -0800

    donzo

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,74 @@
 // Script.js
 
+
+
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+
+  
+
+  fetch("https://fakestoreapi.com/products")
+  .then(response => response.json())
+  .then(data => {
+      localStorage.setItem("list", JSON.stringify(data));
+      
+      var daList = JSON.parse(localStorage.getItem('list'));
+      
+      
+
+      for(var i = 0; i < daList.length; i++) {
+        let toPut = document.createElement('product-item');
+        let current = daList[i];
+        
+        toPut.shadowRoot.querySelector('img').src = current.image;
+        toPut.shadowRoot.querySelector('img').alt = current.description;
+
+        toPut.shadowRoot.querySelector('p').textContent = current.title;
+        toPut.shadowRoot.querySelector('p.price').textContent = current.price
+
+        let cartList = JSON.parse(localStorage.getItem("cart"));
+
+        console.log(cartList)
+
+        if(cartList.includes(toPut.shadowRoot.querySelector('img').src)) {
+          toPut.shadowRoot.querySelector('button').textContent = 'Remove from Cart';
+          document.getElementById('cart-count').innerText = parseInt(document.getElementById('cart-count').innerText) + 1;
+        }
+
+        
+
+        
+        toPut.shadowRoot.querySelector('button').addEventListener('click', () => {
+          if(toPut.shadowRoot.querySelector('button').textContent == 'Add to Cart') {
+            document.getElementById('cart-count').innerText = parseInt(document.getElementById('cart-count').innerText) + 1;
+            toPut.shadowRoot.querySelector('button').textContent = 'Remove from Cart';
+            let a = JSON.parse(localStorage.getItem("cart"));
+            a.push(toPut.shadowRoot.querySelector('img').src);
+            localStorage.setItem("cart", JSON.stringify(a));
+            
+        }
+
+        else if(toPut.shadowRoot.querySelector('button').textContent == 'Remove from Cart') {
+          document.getElementById('cart-count').innerText = parseInt(document.getElementById('cart-count').innerText) - 1;
+          toPut.shadowRoot.querySelector('button').textContent = 'Add to Cart';
+          let a = JSON.parse(localStorage.getItem("cart"));
+            const index = a.indexOf(toPut.shadowRoot.querySelector('img').src);
+            a.splice(index, 1);
+            localStorage.setItem("cart", JSON.stringify(a));
+            
+        }
+        })
+
+        document.getElementById("product-list").append(toPut);
+  }
+    })
+
+  
 });
+
+
+
+
+
+
+
+
